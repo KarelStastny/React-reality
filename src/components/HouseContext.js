af12a653059e7a387ad5c0b9894ccc8b1dd0f1ee
Karel Šťastný <karell29@icloud.com>
2023-07-20 11:28:00 +0200

    Pokračování na projektu

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React,{useState, useEffect, createContext} from 'react';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import {housesData} from "../data"
+
+
+export const HouseContext = createContext()
+
+const HouseContextProvider = ({children}) => {
+  const [houses, setHouses] = useState(housesData)
+  const [country, setCountry] = useState("Location (any)")
+  const [countries, setCountries] = useState([])
+  const [property, setProperty] = useState("Property type (any)")
+  const [properties, setProperties] = useState([])
+  const [price, setPrice] = useState("Price range (any)")
+  const [loading, setLoading] = useState(false)
+
+
+  // retunr all countries
+  useEffect(() => {
+    const allCountries = houses.map((house)=> {
+      return house.country
+    })
+
+    // Remove duplicates
+    const uniqueCountries = ["Location (any)", ... new Set (allCountries)]
+
+    console.log(uniqueCountries);
+
+  })
+
+    
+  return <HouseContext.Provider value={{
+    country,
+    setCountry,
+    countries,
+    property,
+    setProperty,
+    properties,
+    price,
+    setPrice,
+    houses,
+    loading,
+
+  }}>{children}</HouseContext.Provider>;
 };
 
-export default HouseContext;
+export default HouseContextProvider;
